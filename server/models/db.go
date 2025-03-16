b575bb77bea32e8d5f79c3a576dd58add2361162
Junt62 <junt62@qq.com>
2025-03-16 19:54:46 +0800

    update.

--- server/models/db.go
+++ server/models/db.go
@@ -3,7 +3,6 @@ package models
 import (
 	"fmt"
 	"go-chat/config"
-	"log"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -14,11 +13,33 @@ var DB *gorm.DB
 func ConnectDatabase(cfg config.Config) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName)
-
 	var err error
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed to connect to database: ", err)
+		fmt.Println("Datebase doesn't exist, creating now...")
+
+		dsnWithoutDB := fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8mb4&parseTime=True&loc=Local",
+			cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort)
+		dbTemp, err := gorm.Open(mysql.Open(dsnWithoutDB), &gorm.Config{})
+		if err != nil {
+			fmt.Println("Failed to connect to MySQL:", err)
+			return
+		}
+
+		createDBSQL := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS `%s` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;", cfg.DBName)
+		if err := dbTemp.Exec(createDBSQL).Error; err != nil {
+			fmt.Println("Database create failed:", err)
+			return
+		}
+
+		fmt.Println("Database create success！")
+
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			fmt.Println("Database reconnection failed: ", err)
+			return
+		}
+
 	}
 	fmt.Println("Database connection successful: ", cfg.DBHost+":"+cfg.DBPort)
 
--- web/src/main.ts
+++ web/src/main.ts
@@ -1,13 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import ElementPlus from "element-plus";
-import "element-plus/dist/index.css";
+import './assets/main.css'
 
-const app = createApp(App);
-app.use(ElementPlus);
-app.use(store);
-app.use(router);
-app.mount("#app");
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
