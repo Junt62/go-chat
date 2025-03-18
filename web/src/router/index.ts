ec821d8e00e7ebf7ad2fc76d85ded63e75aa8234
zijun <40317014+Junt62@users.noreply.github.com>
2025-03-18 10:36:23 +0000

    update.

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 import ChatRoom from '@/views/ChatRoom.vue'
 import { useUserStore } from '@/stores/user'
 import LoginView from '@/views/LoginView.vue'
@@ -12,14 +13,13 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: AboutView,
+      meta: { requiresAuth: true },
     },
     {
       path: '/register',
--- web/src/stores/user.ts
+++ web/src/stores/user.ts
@@ -1,5 +1,4 @@
 import { defineStore } from 'pinia'
-import { login as apiLogin, logout as apiLogout, ping as apiPing } from '@/api/auth'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
@@ -7,39 +6,15 @@ export const useUserStore = defineStore('user', {
     username: '',
   }),
   actions: {
-    async register(username: string, password: string, email: string) {
-      try {
-        const res = await apiLogin({ username, password })
-        this.token = res.data.token
-        this.username = username
-        localStorage.setItem('token', res.data.token)
-      } catch (error) {
-        throw error
-      }
-    },
-    async login(username: string, password: string) {
-      try {
-        const res = await apiLogin({ username, password })
-        this.token = res.data.token
-        this.username = username
-        localStorage.setItem('token', res.data.token)
-      } catch (error) {
-        throw error
-      }
+    async setToken(token: string, username: string) {
+      this.token = token
+      this.username = username
+      localStorage.setItem('token', token)
     },
     async logout() {
-      await apiLogout()
       this.token = ''
       this.username = ''
       localStorage.removeItem('token')
-    },
-    async ping() {
-      try {
-        const res = await apiPing()
-        console.log(res)
-      } catch (error) {
-        throw error
-      }
     },
   },
 })
