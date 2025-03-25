9753ba62fdae81834ed39c7feb1f0c854af5c68b
zijun <40317014+Junt62@users.noreply.github.com>
2025-03-25 09:34:04 +0000

    update.

--- web/src/components/TopBar.vue
+++ web/src/components/TopBar.vue
@@ -1,64 +1,23 @@
 <!-- src/components/TopBar.vue -->
 <template>
   <el-header class="top-bar">
-    <div class="logo" @click="goToHome">Go-Chat</div>
-    <div class="menu">
-      <div class="menu-item" @click="goToChat">聊天</div>
-      <div class="menu-item" @click="goToSetting">设置</div>
-      <div class="menu-item" @click="goToAbout">关于</div>
-    </div>
-    <!-- <el-menu mode="horizontal" :ellipsis="false" class="menu">
-      <el-menu-item index="1">首页</el-menu-item>
-      <el-menu-item index="2">消息</el-menu-item>
-      <el-menu-item index="3">好友</el-menu-item>
-      <el-menu-item index="4">设置</el-menu-item>
-    </el-menu> -->
+    <div class="logo" @click="goHome">Go-Chat</div>
     <div class="right-tools">
-      <p>{{ userStore.username }}</p>
-      <!-- <el-input v-model="search" placeholder="搜索..." class="search-box">
-        <template #prefix
-          ><el-icon><Search /></el-icon
-        ></template>
-      </el-input> -->
-      <el-switch v-model="darkMode" class="dark-mode-switch" @change="toggleDarkMode">
-        <template #active-action
-          ><el-icon><Moon /></el-icon
-        ></template>
-        <template #inactive-action
-          ><el-icon><Sunny /></el-icon
-        ></template>
-      </el-switch>
+      <el-button type="primary" round class="main-btn" @click="join">立即体验</el-button>
     </div>
   </el-header>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { Search, Moon, Sunny } from '@element-plus/icons-vue'
 import { useRouter } from 'vue-router'
-import { useUserStore } from '@/stores/user'
 
-const search = ref<string>('')
-const darkMode = ref<boolean>(false)
 const router = useRouter()
-const userStore = useUserStore()
 
-const toggleDarkMode = (): void => {
-  darkMode.value = !darkMode.value
-  document.body.classList.toggle('dark-mode', darkMode.value)
-}
-
-const goToHome = () => {
+const goHome = () => {
   router.push('/')
 }
-const goToChat = () => {
+const join = () => {
   router.push('/chat')
-}
-const goToAbout = () => {
-  router.push('/about')
-}
-const goToSetting = () => {
-  router.push('/setting')
 }
 </script>
 
@@ -67,81 +26,33 @@ const goToSetting = () => {
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 10px 20px;
-  /* background: rgba(255, 255, 255, 0.1);
-  backdrop-filter: blur(10px);
-  -webkit-backdrop-filter: blur(10px);
-  border-bottom: 1px solid rgba(255, 255, 255, 0.2); */
   color: white;
   position: fixed;
-  width: 1024px;
+  width: 100%;
   top: 0;
   left: 50%;
   transform: translateX(-50%);
   z-index: 1000;
 }
-
-.el-menu {
-  background: transparent !important;
-  border: none;
-  /* box-shadow: 0 4px 6px rgba(0, 0, 0, 0.5); */
-}
-
-.el-menu-item {
-  color: white !important;
-}
-
-.el-menu-item:hover {
-  background-color: rgba(255, 255, 255, 0.1) !important; /* 更改为你喜欢的颜色 */
-}
-
 .logo {
   font-size: 1.5rem;
   font-weight: bold;
   cursor: pointer;
   transition: color 0.3s;
 }
-
-.logo:hover {
-  color: rgba(255, 255, 255, 0.8);
-}
-
-.menu {
-  display: flex;
-  gap: 20px;
-  justify-content: flex-start;
-  flex: 1;
-  padding-left: 36px;
-  padding-top: 4px;
-}
-
-.menu-item {
-  font-size: 0.9rem;
-  font-weight: 600;
-  cursor: pointer;
-  padding: 12px;
-  transition:
-    color 0.3s,
-    transform 0.3s;
-  color: rgba(255, 255, 255, 0.8);
-}
-
-.menu-item:hover {
-  color: rgba(255, 255, 255, 0.6); /* hover 状态下变色 */
-}
-
 .right-tools {
   display: flex;
   align-items: center;
   gap: 15px;
 }
-
-.search-box {
-  width: 180px;
+.main-btn {
+  background: rgba(255, 255, 255, 0.2);
+  border: none;
+  font-size: 1rem;
+  padding: 12px 24px;
+  transition: all 0.3s;
 }
-
-.dark-mode-switch {
-  --el-switch-on-color: #222;
-  --el-switch-off-color: #f7c948;
+.main-btn:hover {
+  background: rgba(255, 255, 255, 0.4);
 }
 </style>
--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,15 +1,6 @@
 <template>
-  <el-header class="top-bar">
-    <div class="logo" @click="goToHome">Go-Chat</div>
-    <div class="right-tools">
-      <!-- <div @click="ping">{{ userStore.username }}</div> -->
-      <el-button type="primary" round class="sub-btn" v-if="isLoggedIn()" @click="logout"
-        >登出
-      </el-button>
-      <el-button type="primary" round class="sub-btn" v-else @click="login">登录 </el-button>
-    </div>
-  </el-header>
   <div class="app-container">
+    <TopBar />
     <div class="intro-container">
       <div class="content">
         <h1>Go-Chat 在线聊天室</h1>
@@ -20,23 +11,17 @@
             {{ item.text }}
           </el-col>
         </el-row>
-        <el-button type="primary" round class="main-btn" @click="join">立即体验</el-button>
-        <el-button type="primary" round class="main-btn" @click="signUp">用户注册</el-button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useAuth } from '@/composables/useAuth'
-import { useUserStore } from '@/stores/user'
+import TopBar from '@/components/TopBar.vue'
 import { Refresh, User, Check, Edit, Grid, UploadFilled } from '@element-plus/icons-vue'
-import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
 
-const userStore = useUserStore()
 const router = useRouter()
-const { ping, logout } = useAuth()
 
 const features = [
   { icon: Refresh, text: '高性能' },
@@ -46,22 +31,6 @@ const features = [
   { icon: Grid, text: '组件丰富' },
   { icon: UploadFilled, text: '云端部署' },
 ]
-
-const isLoggedIn = (): boolean => {
-  return userStore.username !== ''
-}
-const goToHome = () => {
-  router.push('/')
-}
-const join = () => {
-  router.push('/chat')
-}
-const login = () => {
-  router.push('/login')
-}
-const signUp = () => {
-  router.push('/register')
-}
 </script>
 
 <style scoped>
@@ -78,19 +47,6 @@ const signUp = () => {
   top: 0;
   left: 0;
 }
-
-.top-bar {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  color: white;
-  position: fixed;
-  width: 100%;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  z-index: 1000;
-}
 .features {
   display: flex;
   flex-wrap: wrap;
@@ -103,66 +59,8 @@ const signUp = () => {
   gap: 10px;
   flex: 1 1 10%;
 }
-.logo {
-  font-size: 1.5rem;
-  font-weight: bold;
-  cursor: pointer;
-  transition: color 0.3s;
-}
-.right-tools {
-  display: flex;
-  align-items: center;
-  gap: 15px;
-}
 .content {
   max-width: 600px;
   padding: 20px;
-}
-
-h1 {
-  font-size: 2.5rem;
-  margin-bottom: 10px;
-}
-
-p {
-  font-size: 1.2rem;
-  opacity: 0.9;
-}
-
-ul {
-  list-style: none;
-  padding: 0;
-  font-size: 1.2rem;
-  margin: 20px 0;
-}
-
-li {
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  margin-bottom: 10px;
-}
-.main-btn {
-  background: rgba(255, 255, 255, 0.2);
-  border: none;
-  font-size: 1.2rem;
-  padding: 12px 24px;
-  transition: all 0.3s;
-  margin-top: 60px;
-  margin-left: 40px;
-  margin-right: 40px;
-}
-.main-btn:hover {
-  background: rgba(255, 255, 255, 0.4);
-}
-.sub-btn {
-  background: rgba(255, 255, 255, 0.2);
-  border: none;
-  font-size: 1rem;
-  padding: 12px 24px;
-  transition: all 0.3s;
-}
-.sub-btn:hover {
-  background: rgba(255, 255, 255, 0.4);
 }
 </style>
